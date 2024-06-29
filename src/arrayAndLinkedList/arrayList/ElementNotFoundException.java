957e3f131ba33fc872642f222b834f8aa2bc0a50
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-29 11:11:21 +0300

    реализация

---
+++ src/arrayAndLinkedList/arrayList/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package arrayAndLinkedList.arrayList;
+
+public class ElementNotFoundException extends RuntimeException {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/arrayAndLinkedList/arrayList/NewArrayList.java
@@ -0,0 +1,26 @@
+package arrayAndLinkedList.arrayList;
+
+import java.util.Comparator;
+
+/*
+ *  Методы – добавить элемент, добавить элемент по индексу,
+ * получить элемент, удалить элемент, очистить всю коллекцию, отсортировать,
+ * остальное по желанию
+ * */
+public interface NewArrayList<T> {
+    public boolean add(T element);
+
+    public void add(int index, T element);
+
+    public T get(int index);
+
+    public boolean remove(T element);
+
+    public void clear();
+
+    public void sort(Comparator<T> comparator);
+
+    public int size();
+
+    public Object[] toArray();
+}
