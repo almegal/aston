432dcd6325bfb40752675d560422813da1c3d02f
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-30 23:52:27 +0300

    удаление кастомного исклчения

--- src/arrayAndLinkedList/ArrayList/ElementNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package arrayAndLinkedList.ArrayList;
-
-public class ElementNotFoundException extends RuntimeException {
-    public ElementNotFoundException(String message) {
-        super(message);
-    }
-}
